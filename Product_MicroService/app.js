86c6ebe9fa0d535d1511da85b38fd5f0e2247fa2
Kauhsik Talluri <kaushik.talluri@gmail.com>
2021-04-23 21:35:43 +0530

    Added user friendly console.log

--- Product_MicroService/app.js
+++ Product_MicroService/app.js
@@ -21,10 +21,22 @@ mongoose.connect(MONGO_CONNECTION_URL, {
 .then(() => console.log("Connected to database in cluster"))
 .catch((err) => console.log(err));
 
+/* #### More data hardcoded into mongoDB collection ####
+product.create({
+    "productId": "12445dsd234",
+ "category": "Modile",
+ "productName": "Samsung",
+ "productModel": "GalaxyNote",
+"price": 700,
+"availableQuantity": 10
+})
+*/
+
 
 //app.get('/rest/v1/products', ProductsList) // without explicitly creating routes
 routes(app);
 
 app.listen(PORT, HOSTNAME, () => {
-    console.log(`Server is running at http://${HOSTNAME}:${PORT}/`);
+    console.log(`Connected to port ${PORT}`);
+    console.log(`GET request @ http://${HOSTNAME}:${PORT}/rest/v1/products`);
 })
--- Product_MicroService/config/index.js
+++ Product_MicroService/config/index.js
@@ -5,5 +5,4 @@ dotenv.config()
 module.exports = {
     MONGO_CONNECTION_URL:
     'mongodb+srv://m1000:m1000@cluster0.cgvq9.mongodb.net/Products?retryWrites=true&w=majority',
-   
   };
